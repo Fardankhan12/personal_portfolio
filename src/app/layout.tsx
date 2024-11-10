fd7bbe0b062022e4d405ed51b55a2f959343997f
fardan Khan <khanfardan7792@gmail.com>
2024-11-10 19:22:39 +0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import TransitionProvider from './components/TransitionProvider';
 import type { Metadata } from 'next';
 import localFont from 'next/font/local';
 import './globals.css';
+import React from 'react';
+
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
